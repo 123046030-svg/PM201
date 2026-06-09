d6c226208c4d723b2dcb130d4cb394c68aa13884
Re <123046030@upq.edu.mx>
2026-06-08 23:47:29 -0600

    estilos, perfil.js

--- Practica5/miApp2/components/Perfil.js
+++ Practica5/miApp2/components/Perfil.js
@@ -2,30 +2,18 @@ import { View, Text, StyleSheet } from 'react-native';
 
 export const Perfil = () => {
   return (
-    <View style={styles.perfil}>
-      <Text style={styles.texto}>Nombre: Regina Cortes Vargas</Text>
+    <View>
+      <Text>Nombre: Regina Cortes Vargas</Text>
 
-      <Text style={styles.texto}>
+      <Text>
         Carrera: Ingeniería en Sistemas Computacionales
-      </Text>
+    </Text>
 
-      <Text style={styles.texto}>
+      <Text>
         Materia: Programación Móvil
       </Text>
 
-      <Text style={styles.texto}>Cuatrimestre: 9</Text>
+      <Text>Cuatrimestre: 9</Text>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  perfil: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-
-  texto: {
-    fontSize: 16,
-    marginBottom: 6,
-  },
-});
